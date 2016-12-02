3b0f22f374e05b18af5821cb0b81a245c583ea0c
stiangao <tiangao1102@163.com>
2016-12-03 01:49:00 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,9 +2,8 @@
 
 if [ -f "/data/Gemfile" ]; then
 	echo "> /usr/local/bin/bundle install"
-	/usr/local/bin/bundle install
+	/usr/local/bin/bundle install && /usr/local/bin/bundle exec /usr/local/bin/jekyll build —safe
+elfi	
+	echo "> /usr/local/bin/jekyll build --safe"
+	/usr/local/bin/jekyll build --safe
 fi
-	
-echo "> /usr/local/bin/jekyll build --safe"
-/usr/local/bin/jekyll build --safe
-
